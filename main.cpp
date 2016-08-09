4eff3aaff1de4723cf37ac5640667768d89671a1
Neuromancer2701 <king.seth@gmail.com>
2016-08-09 16:21:43 -0400

    stated working on UDP demo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
+#include "network-socket/UDPSocket.h"
+#include "network-socket/Endpoint.h"
 
 DigitalOut led1(LED1);
+
+const int BROADCAST_PORT = 58083;
 
 // main() runs in its own thread in the OS
 // (note the calls to Thread::wait below for delays)
@@ -11,10 +15,29 @@ int main()
     EthernetInterface eth;
     eth.connect();
 
-    printf("TCP client IP Address is %s\r\n", eth.get_ip_address());
+    printf("Controller IP Address is %s\r\n", eth.get_ip_address());
+
+    UDPSocket sock;
+    sock.init();
+    sock.bind(BROADCAST_PORT);
+    sock.set_broadcasting();
+
+    Endpoint transmit;
+    Endpoint receive;
+    receive.set_address("255.255.255.255", BROADCAST_PORT);
+
+    char out_buffer[] = "very important data";
+    char buffer[256];
 
     while (true)
     {
+        sock.sendTo(transmit, out_buffer, sizeof(out_buffer));
+
+        printf("\nWait for packet...\n");
+        int n = sock.receiveFrom(receive, buffer, sizeof(buffer));
+        buffer[n] = '\0';
+        printf("Packet from \"%s\": %s\n", receive.get_address(), buffer);
+
         led1 = !led1;
         Thread::wait(500);
     }
