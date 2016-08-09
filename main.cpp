5b2184583302badb80efb2b61949d2bbd951f3fe
Seth King <king.seth@gmail.com>
2016-08-08 22:24:38 -0400

    Clion settings and integrated ethernet into STF7

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mbed.h"
+#include "EthernetInterface.h"
 
 DigitalOut led1(LED1);
 
 // main() runs in its own thread in the OS
 // (note the calls to Thread::wait below for delays)
-int main() {
-    while (true) {
+int main()
+{
+
+    EthernetInterface eth;
+    eth.connect();
+
+    printf("TCP client IP Address is %s\r\n", eth.get_ip_address());
+
+    while (true)
+    {
         led1 = !led1;
         Thread::wait(500);
     }
