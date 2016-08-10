96e5b5116d5e13d65f9fba5ce582016fa9627569
Neuromancer2701 <king.seth@gmail.com>
2016-08-10 12:08:17 -0400

    updated main to use the correct classes and libraries for udp, it compiles now at least.

--- main.cpp
+++ main.cpp
@@ -1,55 +1,40 @@
 #include "mbed.h"
+#include "lwip-interface/EthernetInterface.h"
 
+#include <string>
 DigitalOut led1(LED1);
 
+
+using std::string;
 const int BROADCAST_PORT = 58083;
 
 int main()
 {
-    while (true)
-    {
-        led1 = !led1;
-        Thread::wait(500);
-    }
-}
-
-
-#if 0
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
-int main()
-{
-
     EthernetInterface eth;
     eth.connect();
 
     printf("Controller IP Address is %s\r\n", eth.get_ip_address());
 
     UDPSocket sock;
-    sock.init();
     sock.bind(BROADCAST_PORT);
     sock.set_broadcasting();
 
-    Endpoint transmit;
-    Endpoint receive;
-    receive.set_address("255.255.255.255", BROADCAST_PORT);
+    SocketAddress transmit("255.255.255.255", BROADCAST_PORT);
+    SocketAddress receive;
 
-    char out_buffer[] = "very important data";
+    string out_buffer = "very important data";
     char buffer[256];
 
     while (true)
     {
-        sock.sendTo(transmit, out_buffer, sizeof(out_buffer));
+        sock.sendto(transmit, out_buffer.c_str(), out_buffer.size());
 
         printf("\nWait for packet...\n");
-        int n = sock.receiveFrom(receive, buffer, sizeof(buffer));
+        int n = sock.recvfrom(&receive, buffer, sizeof(buffer));
         buffer[n] = '\0';
-        printf("Packet from \"%s\": %s\n", receive.get_address(), buffer);
+        printf("Packet from \"%s\": %s\n", receive.get_ip_address(), buffer);
 
         led1 = !led1;
         Thread::wait(500);
     }
 }
-
-#endif
-
