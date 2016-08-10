d23cfbf0fa36c5ff94d3ebf676deebc4f22f0d9f
Neuromancer2701 <king.seth@gmail.com>
2016-08-10 13:56:57 -0400

    added rtos threads for send and receive

--- main.cpp
+++ main.cpp
@@ -8,32 +8,56 @@ DigitalOut led1(LED1);
 using std::string;
 const int BROADCAST_PORT = 58083;
 
+
+
+UDPSocket socket_g;
+
+void transmit()
+{
+    string out_buffer = "very important data";
+    SocketAddress transmit("255.255.255.255", BROADCAST_PORT);
+
+    while (true)
+    {
+        socket_g.sendto(transmit, out_buffer.c_str(), out_buffer.size());
+        Thread::wait(500);
+    }
+}
+
+void receive()
+{
+    SocketAddress receive;
+    char buffer[256];
+    while (true)
+    {
+        printf("\nWait for packet...\n");
+        int n = socket_g.recvfrom(&receive, buffer, sizeof(buffer));
+        if (n > 0)
+        {
+            buffer[n] = '\0';
+            printf("Packet from \"%s\": %s\n", receive.get_ip_address(), buffer);
+        }
+        Thread::wait(500);
+    }
+}
+
+
 int main()
 {
+    Thread transmitter;
+    Thread receiver;
     EthernetInterface eth;
     eth.connect();
 
     printf("Controller IP Address is %s\r\n", eth.get_ip_address());
 
-    UDPSocket sock;
-    sock.bind(BROADCAST_PORT);
-    sock.set_broadcasting();
-
-    SocketAddress transmit("255.255.255.255", BROADCAST_PORT);
-    SocketAddress receive;
-
-    string out_buffer = "very important data";
-    char buffer[256];
+    socket_g.bind(BROADCAST_PORT);
+    socket_g.set_broadcasting();
+    transmitter.start(transmit);
+    receiver.start(receive);
 
     while (true)
     {
-        sock.sendto(transmit, out_buffer.c_str(), out_buffer.size());
-
-        printf("\nWait for packet...\n");
-        int n = sock.recvfrom(&receive, buffer, sizeof(buffer));
-        buffer[n] = '\0';
-        printf("Packet from \"%s\": %s\n", receive.get_ip_address(), buffer);
-
         led1 = !led1;
         Thread::wait(500);
     }
